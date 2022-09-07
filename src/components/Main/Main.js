042f0dae863884c2f5a1e8b87bb25ffb1fa01cd6
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-09-07 21:32:20 +0300

    Добавить блок о студенте

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import Promo from '../Main/Promo/Promo';
 import NavTab from '../Main/NavTab/NavTab';
 import AboutProject from '../Main/AboutProject/AboutProject';
 import Techs from '../Main/Techs/Techs';
+import AboutMe from '../Main/AboutMe/AboutMe';
 
 function Main() {
   return (
@@ -10,6 +11,7 @@ function Main() {
       <NavTab></NavTab>
       <AboutProject></AboutProject>
       <Techs></Techs>
+      <AboutMe></AboutMe>
     </main>
   );
 }
